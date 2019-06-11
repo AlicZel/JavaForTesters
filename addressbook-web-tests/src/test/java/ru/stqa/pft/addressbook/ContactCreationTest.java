b3bb863c34c1465bd8d8130b7156624d40864137
alicjaz <alicja.zeler@comarch.pl>
2019-06-11 13:53:32 +0200

    Refactor: parametrization all data of Contact form (ContactData object)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -1,12 +1,11 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.*;
 
 public class ContactCreationTest {
   private WebDriver driver;
@@ -21,7 +20,13 @@ public class ContactCreationTest {
   public void testContactCreation() throws Exception {
     login();
     gotoNewContactPage("add new");
-    fillNewContactForm();
+    fillNewContactForm(new ContactData("Alicja", "Katarzyna", "Zeler", "AliZel",
+            "D:\\SzkolenieJavaDlaTesterów\\JavaForTesters\\addressbook-web-tests\\beznazwy.png",
+            "Mrs", "COMARCH", "Guderskiego 1/4\nGdańsk", "504123123",
+            "504123123", "504123123", "504123123", "ala@wp.pl", "ala1@wp.pl",
+            "ala2@wp.pl", "www.wp.pl", "11", "November", "1986",
+            "17", "November", "1986", "d",
+            "Piekna 2\nEłk", "508456456", "uwaga"));
     submitNewContactCreation("//input[@name='submit']");
     returnToHomePage("home page");
     logout("Logout");
@@ -39,39 +44,38 @@ public class ContactCreationTest {
     driver.findElement(By.xpath(s)).click();
   }
 
-  private void fillNewContactForm() {
-    driver.findElement(By.name("firstname")).sendKeys("Alicja");
-    driver.findElement(By.name("middlename")).sendKeys("Katarzyna");
-    driver.findElement(By.name("lastname")).sendKeys("Zeler");
-    driver.findElement(By.name("nickname")).sendKeys("AliZel");
-    driver.findElement(By.name("photo")).sendKeys("D:\\SzkolenieJavaDlaTesterów\\JavaForTesters\\addressbook-web" +
-            "-tests\\beznazwy.png");
-    driver.findElement(By.name("title")).sendKeys("Mrs");
-    driver.findElement(By.name("company")).sendKeys("COMARCH");
-    driver.findElement(By.name("address")).sendKeys("Guderskiego 1/4\nGdańsk");
-    driver.findElement(By.name("home")).sendKeys("504123123");
-    driver.findElement(By.name("mobile")).sendKeys("504123123");
-    driver.findElement(By.name("work")).sendKeys("504123123");
-    driver.findElement(By.name("fax")).sendKeys("504123123");
-    driver.findElement(By.name("email")).sendKeys("ala@wp.pl");
-    driver.findElement(By.name("email2")).sendKeys("ala1@wp.pl");
-    driver.findElement(By.name("email3")).sendKeys("ala2@wp.pl");
-    driver.findElement(By.name("homepage")).sendKeys("www.wp.pl");
+  private void fillNewContactForm(ContactData contactData) {
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getName());
+    driver.findElement(By.name("middlename")).sendKeys(contactData.getSecondName());
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
+    driver.findElement(By.name("nickname")).sendKeys(contactData.getNick());
+    driver.findElement(By.name("photo")).sendKeys(contactData.getPath());
+    driver.findElement(By.name("title")).sendKeys(contactData.getTitle());
+    driver.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    driver.findElement(By.name("home")).sendKeys(contactData.getHomeTel());
+    driver.findElement(By.name("mobile")).sendKeys(contactData.getMobileTel());
+    driver.findElement(By.name("work")).sendKeys(contactData.getWorkTel());
+    driver.findElement(By.name("fax")).sendKeys(contactData.getFax());
+    driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    driver.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
+    driver.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
+    driver.findElement(By.name("homepage")).sendKeys(contactData.getHomepage());
     driver.findElement(By.name("bday")).click();
-    new Select(driver.findElement(By.name("bday"))).selectByVisibleText("11");
+    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(contactData.getBirthDay());
     driver.findElement(By.name("bmonth")).click();
-    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText("November");
-    driver.findElement(By.name("byear")).sendKeys("1986");
+    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBirthMonth());
+    driver.findElement(By.name("byear")).sendKeys(contactData.getBirthYear());
     driver.findElement(By.name("aday")).click();
-    new Select(driver.findElement(By.name("aday"))).selectByVisibleText("17");
+    new Select(driver.findElement(By.name("aday"))).selectByVisibleText(contactData.getAnniversaryDay());
     driver.findElement(By.name("amonth")).click();
-    new Select(driver.findElement(By.name("amonth"))).selectByVisibleText("November");
-    driver.findElement(By.name("ayear")).sendKeys("1986");
+    new Select(driver.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAnniversaryMonth());
+    driver.findElement(By.name("ayear")).sendKeys(contactData.getAnniversaryYear());
     driver.findElement(By.name("new_group")).click();
-    new Select(driver.findElement(By.name("new_group"))).selectByVisibleText("d");
-    driver.findElement(By.name("address2")).sendKeys("Piekna 2\nEłk");
-    driver.findElement(By.name("phone2")).sendKeys("508456456");
-    driver.findElement(By.name("notes")).sendKeys("uwaga");
+    new Select(driver.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+    driver.findElement(By.name("address2")).sendKeys(contactData.getSecondAddress());
+    driver.findElement(By.name("phone2")).sendKeys(contactData.getSecondAddressPhone());
+    driver.findElement(By.name("notes")).sendKeys(contactData.getNotes());
   }
 
   private void gotoNewContactPage(String s) {
@@ -89,7 +93,6 @@ public class ContactCreationTest {
   public void tearDown() throws Exception {
     driver.quit();
   }
-
 
 
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,163 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String secondName;
+  private final String surname;
+  private final String nick;
+  private final String path;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String homeTel;
+  private final String mobileTel;
+  private final String workTel;
+  private final String fax;
+  private final String email;
+  private final String email2;
+  private final String email3;
+  private final String homepage;
+  private final String birthDay;
+  private final String birthMonth;
+  private final String birthYear;
+  private final String anniversaryDay;
+  private final String anniversaryMonth;
+  private final String anniversaryYear;
+  private final String group;
+  private final String secondAddress;
+  private final String secondAddressPhone;
+  private final String notes;
+
+  public ContactData(String name, String secondName, String surname, String nick, String path, String title, String company, String address, String homeTel, String mobileTel, String workTel, String fax, String email, String email2, String email3, String homepage, String birthDay, String birthMonth, String birthYear, String anniversaryDay, String anniversaryMonth, String anniversaryYear, String group, String secondAddress, String secondAddressPhone, String notes) {
+    this.name = name;
+    this.secondName = secondName;
+    this.surname = surname;
+    this.nick = nick;
+    this.path = path;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.homeTel = homeTel;
+    this.mobileTel = mobileTel;
+    this.workTel = workTel;
+    this.fax = fax;
+    this.email = email;
+    this.email2 = email2;
+    this.email3 = email3;
+    this.homepage = homepage;
+    this.birthDay = birthDay;
+    this.birthMonth = birthMonth;
+    this.birthYear = birthYear;
+    this.anniversaryDay = anniversaryDay;
+    this.anniversaryMonth = anniversaryMonth;
+    this.anniversaryYear = anniversaryYear;
+    this.group = group;
+    this.secondAddress = secondAddress;
+    this.secondAddressPhone = secondAddressPhone;
+    this.notes = notes;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSecondName() {
+    return secondName;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public String getNick() {
+    return nick;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomeTel() {
+    return homeTel;
+  }
+
+  public String getMobileTel() {
+    return mobileTel;
+  }
+
+  public String getWorkTel() {
+    return workTel;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+
+  public String getBirthDay() {
+    return birthDay;
+  }
+
+  public String getBirthMonth() {
+    return birthMonth;
+  }
+
+  public String getBirthYear() {
+    return birthYear;
+  }
+
+  public String getAnniversaryDay() {
+    return anniversaryDay;
+  }
+
+  public String getAnniversaryMonth() {
+    return anniversaryMonth;
+  }
+
+  public String getAnniversaryYear() {
+    return anniversaryYear;
+  }
+
+  public String getGroup() {
+    return group;
+  }
+
+  public String getSecondAddress() {
+    return secondAddress;
+  }
+
+  public String getSecondAddressPhone() {
+    return secondAddressPhone;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
