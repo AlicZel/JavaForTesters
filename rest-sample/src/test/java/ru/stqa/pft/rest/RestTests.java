ba06715d591f1e0f8aaf3145ac0efc24916a3ed5
alicjaz <alicja.zeler@comarch.pl>
2019-08-01 12:44:50 +0200

    po 4 lekcji z rozdziału 9

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -1,43 +1,52 @@
 package ru.stqa.pft.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.util.Set;
-
-import org.apache.http.HttpHost;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
-import org.testng.Assert;
+import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.*;
-
-import static com.sun.javafx.runtime.async.BackgroundExecutor.getExecutor;
 import static org.testng.Assert.assertEquals;
 
 public class RestTests {
 
-  @Test
-  public void testCreateIssue() throws IOException {
-    Set<Issue> oldIssues = getIssues();
-    Issue newIssue =  new Issue();
-    Integer issueId= createIssue(newIssue);
-    Set<Issue> newIssues = getIssues();
-    oldIssues.add(newIssue.withId(issueId));
-    assertEquals(newIssues,oldIssues);
+
+    @Test
+    public void testCreateIssue() throws IOException {
+      Set<Issue> oldIssues = getIssues();
+      Issue newIssue =  new Issue().withSubject("Test issue 1").withDescription("new test issue 1");
+      Integer issueId= createIssue(newIssue);
+      Set<Issue> newIssues = getIssues();
+      oldIssues.add(newIssue.withId(issueId));
+      assertEquals(newIssues,oldIssues);
+    }
+
+    private Integer createIssue(Issue newIssue) throws IOException {
+      String json=getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
+      .bodyForm(new BasicNameValuePair("subject",newIssue.getSubject()),
+              new BasicNameValuePair("description",newIssue.getDescription()) ))
+              .returnContent().asString();
+      JsonElement parsed = new JsonParser().parse(json);
+      return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+    private Set<Issue> getIssues() throws IOException {
+      String json=getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
+              .returnContent().asString();
+      JsonParser jsonParser =new JsonParser();
+      JsonElement parsed= jsonParser.parse(json);
+      JsonElement issues =parsed.getAsJsonObject().get("issues");
+      return new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());
+    }
+
+    public Executor getExecutor() {
+      return Executor.newInstance()
+              .auth("f060103b7877858acb33f95ace052441","");
+    }
+
+
   }
-
-  private Integer createIssue(Issue newIssue) {
-    return null;
-  }
-
-  private Set<Issue> getIssues() throws IOException {
-    String json=getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
-            .returnContent().asString();
-    return null;
-  }
-
-  public Executor getExecutor() {
-    return Executor.newInstance()
-            .auth("28accbe43ea112d9feb328d2c00b3eed","");
-  }
-
-
-}
